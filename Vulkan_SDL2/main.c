2cdf7c35dd148b79669ebb4d65acade99cf81e9a
Marcin Zając <marcin@hp.home>
2019-10-08 10:10:11 +0200

    pipeline

--- Vulkan_SDL2/main.c
+++ Vulkan_SDL2/main.c
@@ -11,6 +11,11 @@ static VkSurfaceKHR vulkan_surface = VK_NULL_HANDLE;
 static VkDevice vulkan_device = VK_NULL_HANDLE;
 static VkSwapchainKHR vulkan_swapchain = VK_NULL_HANDLE;
 
+int clamp(int32_t i, int32_t min, int32_t max);
+static size_t fileGetLenght(FILE *file);
+
+VkShaderModule VLK_CreateShaderModule(char *filename);
+
 int main(void)
 {
 	SDL_Init(SDL_INIT_VIDEO);
@@ -19,11 +24,11 @@ int main(void)
 	const char *vulkan_window_title = "Hello Vulkan";
 
 	SDL_Window *vulkan_window 		= SDL_CreateWindow(vulkan_window_title,
-									SDL_WINDOWPOS_UNDEFINED,
-									SDL_WINDOWPOS_UNDEFINED,
-									800,
-									600,
-									SDL_WINDOW_VULKAN); 
+			SDL_WINDOWPOS_UNDEFINED,
+			SDL_WINDOWPOS_UNDEFINED,
+			800,
+			600,
+			SDL_WINDOW_VULKAN); 
 
 	uint32_t instance_extensions_count;
 	SDL_Vulkan_GetInstanceExtensions(vulkan_window, &instance_extensions_count, NULL);
@@ -122,8 +127,8 @@ int main(void)
 
 	int32_t vulkan_window_width, vulkan_window_height;
 	SDL_Vulkan_GetDrawableSize(vulkan_window, &vulkan_window_width, &vulkan_window_height);
-	vulkan_window_width = CLAMP(vulkan_window_width, surface_capabilities.minImageExtent.width, surface_capabilities.maxImageExtent.width);
-	vulkan_window_height = CLAMP(vulkan_window_height, surface_capabilities.minImageExtent.height, surface_capabilities.maxImageExtent.height);
+	vulkan_window_width = clamp(vulkan_window_width, surface_capabilities.minImageExtent.width, surface_capabilities.maxImageExtent.width);
+	vulkan_window_height = clamp(vulkan_window_height, surface_capabilities.minImageExtent.height, surface_capabilities.maxImageExtent.height);
 
 	VkSwapchainCreateInfoKHR swapchain_create_info = {
 		.sType 						= VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR,
@@ -134,8 +139,8 @@ int main(void)
 		.imageFormat 				= swapchain_image_format,
 		.imageColorSpace 			= swapchain_color_space,
 		.imageExtent = {
-			.width 					= 800,
-			.height 				= 600
+			.width 					= vulkan_window_width,
+			.height 				= vulkan_window_height
 		},
 		.imageArrayLayers 			= 1,
 		.imageUsage 				= VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT,
@@ -180,11 +185,122 @@ int main(void)
 				.levelCount 		= 1,
 				.baseArrayLayer 	= 0,
 				.layerCount 		= 1
-				}
-			};
+			}
+		};
 
 		assert(vkCreateImageView(vulkan_device, &image_view_create_info, NULL, &swapchain_images_views[i]) == VK_SUCCESS);
 	}
+
+
+	VkShaderModule vert_shader_module = VLK_CreateShaderModule("./vert.spv");
+	VkShaderModule frag_shader_module = VLK_CreateShaderModule("./frag.spv");
+
+	VkPipelineShaderStageCreateInfo vert_shader_stage_info = {
+		.sType 						= VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO,
+		.stage 						= VK_SHADER_STAGE_VERTEX_BIT,
+		.module 					= vert_shader_module,
+		.pName 						= "main"
+	};
+
+	VkPipelineShaderStageCreateInfo frag_shader_stage_info = {
+		.sType 						= VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO,
+		.stage 						= VK_SHADER_STAGE_FRAGMENT_BIT,
+		.module 					= vert_shader_module,
+		.pName 						= "main"
+	};
+
+	VkPipelineShaderStageCreateInfo shader_stages[] = {vert_shader_stage_info, frag_shader_stage_info};
+
+	VkPipelineVertexInputStateCreateInfo vert_input_info = {
+		.sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO,
+		.vertexBindingDescriptionCount = 0,
+		.pVertexBindingDescriptions = NULL,
+		.vertexAttributeDescriptionCount = 0,
+		.pVertexAttributeDescriptions = NULL
+	};
+
+	VkPipelineInputAssemblyStateCreateInfo input_assembly = {
+		.sType = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO,
+		.topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST,
+		.primitiveRestartEnable = VK_FALSE
+	};
+
+	VkPipelineViewportStateCreateInfo viewport_state = {
+		.sType = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO,
+		.viewportCount 				= 1,
+		.pViewports = &(VkViewport) {
+			.x 						= 0.0f,
+			.y 						= 0.0f,
+			.width 					= (float) vulkan_window_width,
+			.height 				= (float) vulkan_window_height,
+			.minDepth 				= 0.0f,
+			.maxDepth 				= 1.0f
+		},
+		.scissorCount 				= 1,
+		.pScissors = &(VkRect2D){
+			.offset = {
+				.x 					= 0,
+				.y 					= 0
+			},
+			.extent = {
+				.width 				= vulkan_window_width,
+				.height 			= vulkan_window_height
+			}
+		}
+	};
+
+	VkPipelineRasterizationStateCreateInfo vulkan_rasterizer = {
+		.sType 						= VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO,
+		.depthClampEnable 			= VK_FALSE,
+		.polygonMode 				= VK_POLYGON_MODE_FILL,
+		.lineWidth 					= 1.0f,
+		.cullMode 					= VK_CULL_MODE_BACK_BIT,
+		.frontFace 					= VK_FRONT_FACE_CLOCKWISE,
+		.depthBiasEnable 			= VK_FALSE,
+		.depthBiasConstantFactor 	= 0.0f,
+		.depthBiasClamp 			= 0.0f,
+		.depthBiasSlopeFactor 		= 0.0f
+	};
+
+	VkPipelineMultisampleStateCreateInfo vulkan_multisampling = {
+		.sType 						= VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO,
+		.sampleShadingEnable 		= VK_FALSE,
+		.rasterizationSamples 		= VK_SAMPLE_COUNT_1_BIT,
+		.minSampleShading 			= 1.0f,
+		.pSampleMask 				= NULL,
+		.alphaToCoverageEnable 		= VK_FALSE,
+		.alphaToOneEnable 			= VK_FALSE
+	};
+
+	VkPipelineColorBlendAttachmentState color_blend_attachment  = {
+		.colorWriteMask 			= VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT | VK_COLOR_COMPONENT_B_BIT | VK_COLOR_COMPONENT_A_BIT,
+		.blendEnable 				= VK_FALSE,
+		.srcColorBlendFactor 		= VK_BLEND_FACTOR_ONE,
+		.dstColorBlendFactor 		= VK_BLEND_FACTOR_ZERO,
+		.colorBlendOp 				= VK_BLEND_OP_ADD,
+		.srcAlphaBlendFactor 		= VK_BLEND_FACTOR_ONE,
+		.dstAlphaBlendFactor 		= VK_BLEND_FACTOR_ZERO,
+		.alphaBlendOp 				= VK_BLEND_OP_ADD
+	};
+
+
+	VkPipelineLayoutCreateInfo pipeline_layout_info = {
+		.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO,
+		.setLayoutCount = 0, // Optional
+		.pSetLayouts = NULL, // Optional
+		.pushConstantRangeCount = 0, // Optional
+		.pPushConstantRanges = NULL  // Optional
+	};
+
+	VkPipelineLayout pipeline_layout = VK_NULL_HANDLE;
+	assert(vkCreatePipelineLayout(vulkan_device, &pipeline_layout_info, NULL, &pipeline_layout) == VK_SUCCESS);
+
+	VkAttachmentDescription color_attachment = {
+		.format = swapchain_image_format,
+		.samples = VK_SAMPLE_COUNT_1_BIT,
+		.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR,
+		.storeOp = VK_ATTACHMENT_STORE_OP_STORE
+	};
 
 	bool quit = false;
 	SDL_Event event;
@@ -199,11 +315,71 @@ int main(void)
 	for (uint32_t i = 0; i < swapchain_images_count; ++i) {
 		vkDestroyImageView(vulkan_device, swapchain_images_views[i], NULL);
 	}
+
+	vkDestroyPipelineLayout(vulkan_device, pipeline_layout, NULL);
+	vkDestroyShaderModule(vulkan_device, vert_shader_module, NULL);
+	vkDestroyShaderModule(vulkan_device, frag_shader_module, NULL);
+
 	vkDestroySwapchainKHR(vulkan_device, vulkan_swapchain, NULL);
 	vkDestroySurfaceKHR( vulkan_instance, vulkan_surface, NULL);
-    vkDestroyDevice(vulkan_device, NULL);
-    vkDestroyInstance(vulkan_instance, NULL);
+	vkDestroyDevice(vulkan_device, NULL);
+	vkDestroyInstance(vulkan_instance, NULL);
 
 	SDL_Quit();
 	return 0;
+}
+
+static size_t fileGetLenght(FILE *file){
+
+	size_t lenght;
+	size_t currPos = ftell(file);
+	fseek(file, 0 , SEEK_END);
+	lenght = ftell(file);
+	fseek(file, currPos, SEEK_SET);
+	return lenght;
+}
+
+int clamp(int32_t i, int32_t min, int32_t max){
+	if (i < min)
+		return min;
+	if (i > max)
+		return max;
+	return i;
+}
+
+VkShaderModule VLK_CreateShaderModule(char *filename){
+	FILE *file = fopen(filename,"r");
+
+	if (!file) {
+		printf("Can't open file: %s\n", filename);
+		return 0;
+	}
+
+	size_t lenght = fileGetLenght(file);
+
+	const uint32_t *shader_descripteion = (const uint32_t *)calloc(lenght + 1, 1);
+	if (!shader_descripteion) {
+		printf("Out of memory when reading file: %s\n",filename);
+		fclose(file);
+		file = NULL;
+		return 0;;
+	}
+
+	fread((void *)shader_descripteion,1,lenght,file);
+	fclose(file);
+
+	file = NULL;
+
+	VkShaderModuleCreateInfo shader_module_create_info = {
+		.sType 						= VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO,
+		.codeSize 					= lenght,
+		.pCode 						= shader_descripteion
+	};
+
+	VkShaderModule shader_module;
+	assert(vkCreateShaderModule(vulkan_device, &shader_module_create_info, NULL, &shader_module) == VK_SUCCESS); 
+	free((void *)shader_descripteion);
+	shader_descripteion = NULL;
+
+	return shader_module;
 }
